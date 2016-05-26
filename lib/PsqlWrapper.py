d35d5f3675c2d2d08247e1e1c8b95803d6aaf022
makoto kuwata <kwa@kuwata-lab.com>
2016-05-27 02:56:43 +0900

    use '%r' instead of '%s' in logging format
    because:
      >>> '%s %s' % (123, "123")
      '123 123'                  # can't distinguish data type
      >>> '%r %r' % (123, "123")
      "123 '123'"                # can distinguish data type

--- lib/PsqlWrapper.py
+++ lib/PsqlWrapper.py
@@ -24,10 +24,10 @@ class PsqlWrapper:
         self.username = username or os.getenv("PGUSER")     or os.getenv("USER")
         self.dbname   = dbname   or os.getenv("PGDATABASE") or self.username
 
-        log.debug("host:   %s", self.host)
-        log.debug("port:   %s", self.port)
-        log.debug("user:   %s", self.username)
-        log.debug("dbname: %s", self.dbname)
+        log.debug("host:   %r", self.host)
+        log.debug("port:   %r", self.port)
+        log.debug("user:   %r", self.username)
+        log.debug("dbname: %r", self.dbname)
 
         self.on_error_stop = on_error_stop
 
