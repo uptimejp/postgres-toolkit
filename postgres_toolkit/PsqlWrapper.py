41fb9abfeb545506c2d0abf6b76b1fa734f7d6f1
Satoshi Nagayasu <snaga@uptime.jp>
2018-02-01 06:01:38 +0000

    Refactor PsqlWrapper.

--- postgres_toolkit/PsqlWrapper.py
+++ postgres_toolkit/PsqlWrapper.py
@@ -13,18 +13,112 @@ import sys
 import log
 
 
+def parse_version(s):
+    m = re.search('PostgreSQL (\d+\.\d+)', s)
+    ver = float(m.group(1))
+    if ver >= 10:
+        return int(ver)
+    return ver
+
+
+def stdout2resultset(s):
+    lines = s.splitlines()
+    return [line.split('|') for line in lines]
+
+
+def is_row_count_row(row):
+    return (len(row) == 1 and
+            ((row[0].startswith('(') and row[0].endswith(' row)')) or
+             (row[0].startswith('(') and row[0].endswith(' rows)'))))
+
+
+def get_column_widths(rs):
+    widths = None
+    for row in rs:
+        if not widths:
+            # Get initial widths.
+            log.debug(row)
+            widths = [len(col) for col in row]
+            continue
+
+        # ignore the '(? rows)' row.
+        if is_row_count_row(row):
+            continue
+
+        # Update with longer ones.
+        for i, col in enumerate(row):
+            log.debug(row)
+            widths[i] = max(widths[i], len(col))
+    return widths
+
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except Exception as ex:
+        pass
+    return False
+
+
+def format_row(row, widths, is_header=False):
+    if len(row) != len(widths):
+        return None
+
+    buf = []
+    for c, w in zip(row, widths):
+        s = str(c)
+        if is_header:
+            s = s.center(w)
+        elif is_number(s):
+            s = s.rjust(w)
+        else:
+            s = s.ljust(w)
+        buf.append(s)
+    out = '| %s |' % (' | '.join(buf))
+    return out
+
+
+def format_resultset(rs):
+    out_fmt = ''
+    if rs is None:
+        return '(nothing to be displayed)'
+
+    widths = get_column_widths(rs)
+    assert widths
+
+    sep = '-+-'.join(['-' * w for w in widths])
+    sep = '+-%s-+' % sep
+
+    for i, r in enumerate(rs):
+        if len(r) != len(widths):
+            continue
+
+        out = format_row(r, widths, (i == 0))
+
+        if i == 0:
+            out_fmt = out_fmt + sep + '\n'
+            out_fmt = out_fmt + out + '\n'
+            out_fmt = out_fmt + sep + '\n'
+            continue
+
+        out_fmt = out_fmt + out + '\n'
+    out_fmt = out_fmt + sep + '\n'
+    return out_fmt
+
+
 class PsqlWrapper:
     version = None
 
     def __init__(self, host, port, username, dbname, on_error_stop=False,
                  debug=False):
-        if debug is True:
+        if debug:
             log.setLevel(log.DEBUG)
 
-        self.host = host or os.getenv("PGHOST") or "localhost"
-        self.port = int(port or os.getenv("PGPORT") or 5432)
-        self.username = username or os.getenv("PGUSER") or os.getenv("USER")
-        self.dbname = dbname or os.getenv("PGDATABASE") or self.username
+        self.host = host or os.environ.get("PGHOST", "localhost")
+        self.port = int(port or os.environ.get("PGPORT", 5432))
+        self.username = username or os.environ.get("PGUSER", os.getenv("USER"))
+        self.dbname = dbname or os.environ.get("PGDATABASE", self.username)
 
         log.debug("host:   %r" % self.host)
         log.debug("port:   %r" % self.port)
@@ -33,101 +127,53 @@ class PsqlWrapper:
 
         self.on_error_stop = on_error_stop
 
+        self.stdout_data = None
+        self.stderr_data = None
+
     def get_version(self):
-        if self.version is None:
+        if not self.version:
             rs = self.execute_query('select version()')
-#            print(rs[1])
-            m = re.search('PostgreSQL (\d+\.\d+)', rs[1][0])
-
-            self.version = float(m.group(1))
-
+            self.version = parse_version(rs[1][0])
         return self.version
 
-    def execute_query(self, query, ignore_error=False):
+    def psql_cmd(self):
         cmd = "psql -A -h {h} -p {p} -U {U} -d {d}".format(
             h=self.host, p=self.port, U=self.username, d=self.dbname)
-        if self.on_error_stop is True:
+        if self.on_error_stop:
             cmd += " --set=ON_ERROR_STOP=on"
+        return cmd
 
-        log.debug(cmd)
+    def execute_query(self, query, ignore_error=False):
+        self.stdout_data = None
+        self.stderr_data = None
 
-        pipe = subprocess.PIPE
-        p = subprocess.Popen(cmd, shell=True,
-                             stdin=pipe, stdout=pipe, stderr=pipe)
+        log.debug(self.psql_cmd())
+
+        p = subprocess.Popen(self.psql_cmd(),
+                             shell=True,
+                             stdin=subprocess.PIPE,
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE)
+
         stdout_data, stderr_data = p.communicate(input=query)
-
+        self.stdout_data = stdout_data
         self.stderr_data = stderr_data
 
-        if p.returncode != 0:
-            if ignore_error is True:
-                log.debug("return code: " + str(p.returncode))
-                log.debug(stderr_data)
+        log.debug('stdout_data: ' + self.stdout_data)
+        log.debug('stderr_data: ' + self.stderr_data)
+        log.debug("return code: " + str(p.returncode))
+
+        if ((self.stderr_data and self.stderr_data.startswith('ERROR: ')) or
+                p.returncode != 0):
+            if ignore_error:
+                log.error(self.stderr_data.rstrip())
                 return None
             else:
-                log.debug("return code: " + str(p.returncode))
-                log.error("Failed to execute psql. (" + cmd + ")")
-                if stderr_data is not None:
-                    log.error(stderr_data.replace('\n', ''))
+                # stop here.
+                log.error(self.stderr_data.rstrip())
                 sys.exit(1)
 
-        lines = stdout_data.splitlines()
-        rs = [line.split('|') for line in lines]
-
-        return rs
+        return stdout2resultset(self.stdout_data)
 
     def print_result(self, rs):
-        if rs is None:
-            return
-
-        widths = cols = None
-        for r in rs:
-            if widths is None:
-                assert cols is None
-                widths = [len(c) for c in r]
-                cols = len(r)
-            elif len(r) == cols:
-                for i, c in enumerate(r):
-                    widths[i] = max(widths[i], len(c))
-
-#        print widths
-
-        if widths is None:
-            return
-
-        sep = '-+-'.join(['-' * w for w in widths])
-        sep = '+-%s-+' % sep
-
-        header = True
-        for r in rs:
-            if len(r) != cols:
-                continue
-
-            buf = []
-            for c, w in zip(r, widths):
-                s = str(c)
-                s = (s.center(w) if header else
-                     s.rjust(w) if is_number(s) else
-                     s.ljust(w))
-                buf.append(s)
-            out = '| %s |' % (' | '.join(buf))
-
-            if header:
-                print sep
-                print out
-                print sep
-                header = False
-            else:
-                print out
-
-        print sep
-
-
-def is_number(s):
-    # return bool(re.match('^-?\d+$', s))           # integer
-    return bool(re.match('^-?\d+(?:\.\d+)?$', s))  # float
-    # ## or
-    # try:
-    #     float(s)
-    #     return True
-    # except:
-    #     return False
+        print format_resultset(rs)
